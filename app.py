380d84119de8d1b92d4c9541dbf09e300f106df6
renzkie8 <[cacolico@my.cspc.edu.ph]>
2026-05-11 19:04:15 +0800

    Corrected versions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 # Add the 'src' directory to the Python path so imports work correctly
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
-import flet_fastapi
+import flet.fastapi as flet_fastapi
 
 from main import main
 
